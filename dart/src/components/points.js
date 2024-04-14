c021cd47a1a33e965e12de378ff1f8f5fe5570db
Jasmin Fränti <jasmin.franti@edu.turkuamk.fi>
2024-04-14 14:27:37 +0300

    Improved adding points to players, created GameStateContext

--- dart/src/components/points.js
+++ dart/src/components/points.js
@@ -1,4 +1,5 @@
-import { React, useState } from 'react';
+import { React, useState, useContext } from 'react';
+import { TurnContext } from '../contexts/AppStateContext';
 
 const testDataPlayers = [{ name: "Player 1", scores: [301] },
 { name: "Player 2", scores: [301] }];
@@ -9,6 +10,7 @@ const ScoreTable = () => {
   const [p2Points, setP2Points] = useState([301, 281]);
   const player1 = testDataPlayers[0];
   const player2 = testDataPlayers[1];
+  const [currentPlayer, setCurrentPlayer] = useState(player1);
 
   const addPoints = (score, player) => {
 
@@ -17,26 +19,36 @@ const ScoreTable = () => {
     } else {
       setP2Points(prevPoints => [...prevPoints, prevPoints[prevPoints.length - 1] - score]);
     }
-
+    setCurrentPlayer(player === player1.name ? player2 : player1)
   }
   return (
     <>
-
-      <div style={{ display: 'flex', flexDirection: 'column', padding: 10 }}>
-        <h2>Player 1</h2>
-        <ul>
-        {p1Points.map((points, index) => (
-          <p key={index}>{points}</p>
-        ))}
-        </ul>
+      <div style={{ display: 'flex', flexDirection: 'column', margin: 10 }}>
+      <label htmlFor='points'>Enter points for player: {currentPlayer.name}</label>
+        <div style={{padding: 20}}>
+          <input type="number" id='points' placeholder="Enter points" />
+          <button onClick={() => {
+            const points = document.getElementById('points').value;
+            addPoints(points, currentPlayer.name)}}>
+            Add Points</button>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', margin: 10 }}>
+          <div style={{ padding: 20 }}>
+            <h2>Player 1</h2>
+            <ul>
+              {p1Points.map((points, index) => (
+                <p key={index}>{points}</p>
+              ))}
+            </ul>
+          </div>
+          <div style={{ padding: 20 }}>
+            <h2>Player 2</h2>
+            {p2Points.map((points, index) => (
+              <p key={index}>{points}</p>
+            ))}
+          </div>
+        </div>
       </div>
-      <div style={{ display: 'flex', flexDirection: 'column' }}>
-        <h2>Player 2</h2>
-        {p2Points.map((points, index) => (
-          <p key={index}>{points}</p>
-        ))}
-      </div>
-      <button onClick={() => addPoints(20, "Player 1")}> Add Points</button>
     </>
   );
 }
--- dart/src/contexts/AppStateContext.js
+++ dart/src/contexts/AppStateContext.js
@@ -1,4 +1,7 @@
-// Context for the app state
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 export const AppStateContext = createContext('intial state');
+export const GameStateContext = createContext({ 
+    "mode": 301, 
+    "players": [{ name: "Player 1", scores: [301] },
+                { name: "Player 2", scores: [301] }] });
