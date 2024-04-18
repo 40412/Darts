b42b5ebfb9c3841241398fdbd0b44d7007060f88
Jasmin Fränti <jasmin.franti@edu.turkuamk.fi>
2024-04-18 18:14:21 +0300

    Divided components into more components and subcomponents

--- dart/src/App.js
+++ dart/src/App.js
@@ -1,56 +1,27 @@
 import './App.css';
 import { ScoreTable } from './components/ScoreTable.js';
-import { AppStateContext } from './contexts/AppStateContext';
-import { React, useState } from 'react';
+import { AppStateProvider, AppStateContext } from './contexts/AppStateContext.js';
+import { React, useContext, useState } from 'react';
 import { GameProvider } from './contexts/GameContext.js';
-import { GameInfo } from './components/Gameinfo.js';
+import { GameInfo } from './components/GameInfo.js';
+import { ContextButton } from './components/ContextButton.js';
+import { GameSetup } from './components/GameSetup.js';
+import { Game } from './components/Game.js';
+import { Summary } from './components/Summary.js';
 
 function App() {
-  
-  const [appState, setAppState] = useState('initial state');
+
   return (
-  <div className="App">
-    <button onClick={()=>{
-      if (appState === 'game') setAppState('summary')
-      else if (appState === 'summary') setAppState('initial state')
-      else
-      setAppState('game')
-    }}>
-      Click to change context</button>
-    
-    <h1>
-    Welcome to Darts!
-    </h1>
-    <AppStateContext.Provider value={{appState, setAppState}}>
-    <GameProvider>
-    {appState === 'initial state' && (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 
-          'center', height: '100vh' }}>
-      {/* Render additional components for 'initial state' */}
-      <p>Player name inputs, game mode selection etc. are going to be here</p>
-    </div>
-    )}
-    {appState === 'game' && (
-    <div style={{ display: 'flex', flexDirection: 'column', 
-                justifyContent: 'center', alignItems: 'center' }}>
-        
-          <GameInfo />
-          <ScoreTable />
-        
-        
-    </div>
-    )}
-    {appState === 'summary' && (
-    <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 
-          'center', height: '100vh' }}>
-      {/* Render additional components for 'summary' */}
-      <h2>Summary of the game here</h2>
-      <p>Winner, legs, something here</p>
-    </div>
-    )}
-    </GameProvider>
-    </AppStateContext.Provider>
-  </div>
+    <div className="App">
+      <h1>
+        Welcome to Darts!
+      </h1>
+      <AppStateProvider>
+      <GameSetup />
+      <Game />
+      <Summary />
+      </AppStateProvider>
+    </div >
   );
 }
 
--- dart/src/contexts/AppStateContext.js
+++ dart/src/contexts/AppStateContext.js
@@ -1,5 +1,12 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-export const AppStateContext = createContext('intial state');
+export const AppStateContext = createContext('initial state');
 
-    
+export const AppStateProvider = ({ children }) => {
+    const [appState, setAppState] = useState('initial state');
+    return (
+      <AppStateContext.Provider value={{ appState, setAppState }}>
+        {children}
+      </AppStateContext.Provider>
+    );
+  };
